e3ccfa083a25ce9247b6dc509d5d824da4f2a492
spurs0929 <briankang0929@gmail.com>
2022-07-21 16:52:42 +0800

    add getScrollOffset

--- DOM.js
+++ DOM.js
@@ -16,3 +16,17 @@ function elemChildren(node){
   
   return temp;
 }
+
+function getScrollOffset(){
+  if(window.pageXOffset){
+    return {
+      left: window.pageXOffset,
+      top: window.pageYOffset
+    }
+  }else{
+    return {
+      left: document.body.scrollLeft + document.documentElement.scrollLeft,
+      top: document.body.scrollTop + document.documentElement.scrollTop 
+    }
+  }
+}
